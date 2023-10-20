9effc0786f911e3b420d78c91ca85adbee51b6d5
Vaibhav Barot <vaibhavbarot04@gmail.com>
2023-10-19 21:51:47 -0400

    more test cases

--- spec/support/routeSpec.spec.js
+++ spec/support/routeSpec.spec.js
@@ -10,7 +10,7 @@ describe('Test endpoints', () => {
 
     });
 
-    it('return HTTP 404 for non existing routes', async () => {
+    it('should return HTTP 404 for non existing routes', async () => {
         try {
             await axios
             .get(root + '/invalidRoute')
@@ -19,4 +19,42 @@ describe('Test endpoints', () => {
             expect(e.response.status).toBe(404);
         }
       });
+
+      it('should create a new user account', async () => {
+        const res = await axios.post(root + 'sign-up',{
+            emailIdInput:'test@' + Math.random() + '.com',
+            passwordInput:'test@123',
+            firstName:'Test',
+            lastName:'Test'
+        })
+        expect(res.status).toBe(200);
+      })
+
+      it('should sign into an account', async () => {
+        const res = await axios.post(root + 'sign-in', {
+            emailIdInput:'vb@vb.com',
+            passwordInput:'vb'
+        })
+        expect(res.status).toBe(200);
+      })
+
+      it('should sign out of an account', async () => {
+        const res = await axios.get(root + 'sign-out')
+        expect(res.status).toBe(200);
+      })
+
+      it('load create event page', async () => {
+        const res = await axios.get(root + 'create-event')
+        expect(res.status).toBe(200);
+      })
+
+      it('should create an event', async () => {
+        const res = await axios.post(root + 'create-event',{
+            date:'test@' + Math.random() + '.com',
+            name:'test@123',
+            time:'Test',
+            venue:'Test'
+        })
+        expect(res.status).toBe(200);
+      })
 });
