26233ce2b7c986054b463887e10dc0cf30565e0a
ShashankRKumar <63204698+ShashankRKumar@users.noreply.github.com>
2023-11-14 18:58:32 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const home = require('./home');
-const events = require('./events')
+const events = require('./events');
+const reports = require('./reports');;
 
 const constructorMethod = (app) =>{
     app.use('/',home);
     app.use('/allevents',events);
+    app.use("/reports", reports);
     app.use('*', (req, res) => {
         return res.status(404).render('./errorPage', {title: "Error", error: "Oops! the page you are searching doesn't exist"});
         
