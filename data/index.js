45a6c7fcaf646c5bcdb2aa5b62bcb0af1bfe01cd
ShashankRKumar <63204698+ShashankRKumar@users.noreply.github.com>
2023-11-14 21:56:03 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,9 +1,7 @@
 const users = require('./users');
 const events = require('./events');
-const reports = require('./reports');
 
 module.exports = {
   users: users,
-  events: events,
-  reports: reports
+  events: events
 };
